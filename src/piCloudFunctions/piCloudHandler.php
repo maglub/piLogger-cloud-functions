5e99d055b99861532833cfe8e287c050b90e0b00
Dominic Schlegel <info@dominicschlegel.ch>
2015-06-15 11:12:17 +0200

    refactor to work only with start and endtime instead of array

--- src/piCloudFunctions/piCloudHandler.php
+++ src/piCloudFunctions/piCloudHandler.php
@@ -64,47 +64,71 @@ class piCloudHandler {
    // get data for sensorid and year
    function getDataBySensorYear($sensorId, $year){
 	    
-      // generate an array with all days for the given year
-      $allDays = $this->generateDayArray($year);
+      $start = \Carbon\Carbon::create($year)->startOfYear();
+      $end = \Carbon\Carbon::create($year)->endOfYear();
 
       // call function to generate json and return array back to user
-     	return $this->getJSONDataPointsAsync($sensorId, $allDays);
+      return $this->getJSONDataPointsAsync($sensorId, $start, $end);
    }
    
    // get data for sensorid and month
    function getDataBySensorMonth($sensorId, $year, $month){
 	    
-      // generate an array with all days for the given year
-      $allDays = $this->generateDayArray($year, $month);
+      // define start and end of the time window
+      $start = \Carbon\Carbon::create($year,$month)->startOfMonth();
+      $end = \Carbon\Carbon::create($year,$month)->endOfMonth();
 
       // call function to generate json and return array back to user
-     	return $this->getJSONDataPointsAsync($sensorId, $allDays);
+      return $this->getJSONDataPointsAsync($sensorId, $start, $end);
    }
 
    // get data for sensorid and specific day
    function getDataBySensorDay($sensorId, $year, $month, $day){
-	    
-      // generate an array with all days for the given year
-      $allDays = $this->generateDayArray($year, $month, $day);
-
+	   
+	   // define start and end of the time window 
+      $start = \Carbon\Carbon::create($year,$month,$day)->startOfDay();
+      $end = \Carbon\Carbon::create($year,$month,$day)->endOfDay();
+      
       // call function to generate json and return array back to user
-     	return $this->getJSONDataPointsAsync($sensorId, $allDays);
+      return $this->getJSONDataPointsAsync($sensorId, $start, $end);
    }
 
    
+   
    // this function queries cassandra DB and returns a json
-   function getJSONDataPointsAsync($sensorId, $allDaysArray){
+   function getJSONDataPointsAsync($sensorId, $starttime, $endtime){
 		
       // prepare the SQL statement
-      $statement = $this->cassandraConnection->prepare("SELECT probe_time, probe_value FROM sensordata WHERE sensor_id = ? and day = ?");
+      $statement = $this->cassandraConnection->prepare("SELECT probe_time, probe_value FROM sensordata WHERE sensor_id = ? and day = ? and probe_time >= ? and probe_time <= ?");
 		
-      // empty array to hold query results
-      $futures = array();
-
-      // execute all statements in background
-      foreach ($allDaysArray as $current_day) {
+      // save original endtime  
+      $original_endtime = clone $endtime;  
+		
+      // loop as long as starttime is lower than endtime  
+	   while($starttime->lt($original_endtime)){
+         
+         // if the endtime is on the same day as the new starttime we use the original time
+         if( $original_endtime->isSameDay($starttime) ){
+            $endtime = $original_endtime;
+         }else{
+            $endtime = clone $starttime;
+            $endtime = $endtime->endOfDay();
+         }
+         
+         
+         // prepare values for cassandra execution option object
+         $curday = $starttime->toDateString(); 
+         $startvalue = new \Cassandra\Timestamp(strtotime($starttime->toDateTimeString()));
+         $endvalue = new \Cassandra\Timestamp(strtotime($endtime->toDateTimeString()));
+         
          // execute statement async and with exec option and hold result in array
-         $futures[]= $this->cassandraConnection->executeAsync($statement, new \Cassandra\ExecutionOptions(array('arguments' => array($sensorId, $current_day))));
+         $futures[]= $this->cassandraConnection->executeAsync($statement, new \Cassandra\ExecutionOptions(array('arguments' => array($sensorId, $curday, $startvalue, $endvalue))));
+         
+         // set new start time
+         $starttime = $starttime->addDay()->startOfDay();
+         
+
+         
       }
       
       // wait for all statements to complete
@@ -121,9 +145,10 @@ class piCloudHandler {
       }
 	
       return array('sensor' => $this->getSensorNameById($sensorId),$this->getSensorTypeById($sensorId) => $data );
+
    }
-    
-      
+		   
+	      
    // get a sensor name by a given sensor id 
    function getSensorNameById($sensorId){
        
@@ -154,94 +179,6 @@ class piCloudHandler {
 
       return $row[0];   
    }
-   
-    
-   // returns an array with all days for the given period from parameters
-   function generateDayArray($year, $month=null, $day=null){
-		 
-      // create new empty array to hold all days
-      $days = array();
- 
-      // we have just a year param
-      if(is_null($day) and is_null($month)){
-
-         $startday = \Carbon\Carbon::create($year)->startOfYear();
-         $endday = \Carbon\Carbon::create($year)->lastOfYear();	
-		
-      // we have a year and a month set	 
-      }elseif(is_null($day) and !is_null($month)){
-		
-         $startday = \Carbon\Carbon::create($year,$month)->firstOfMonth();
-         $endday = \Carbon\Carbon::create($year,$month)->lastOfMonth();
-		
-      // we have all 3 params 
-      }else{
-         
-         array_push($days, \Carbon\Carbon::create($year,$month,$day)->toDateString());
-         return $days;	
-      }	
-		
-      // iterate as long as the cur day is less or equals the end day
-      while($startday->lt($endday)){
-         array_push($days, $startday->toDateString());
-         $startday = $startday->addDay();
-      }	 
-		 
-      return $days;	 
-	}
-
-   
-   function generateDayArrayForDaysBack($daysSince){
-      
-       // create new empty array to hold all days
-      $days = array();
-      
-      // create timenow and timeago objects
-      $timenow = new \Carbon\Carbon(); 
-      $timeago = clone $timenow;
-      $timeago = $timeago->subDay($daysSince);
-      
-      
-      // iterate as long as the cur day is less or equals the end day
-      while($startday->lt($endday)){
-         array_push($days, $startday->toDateString());
-         $startday = $startday->addDay();
-      }	 
-		 
-      return $days;	 
-   } 
-    
-    
-   // get all data from all sensor for the plot that has the given name
-   function getDataByGraphName($graphName){
-	    
-	   $sensorAndTime = $this->getSensorsAndWindowForGraphName($graphName);   
-	    
-   }
-    
-   
-   
-    
-    
-   function getSensorsAndWindowForGraphName($graphName){
-		
-      // prepare SQL statement
-      $stmt = $this->mysqlConnection->prepare('select s.identifier, g.dataSinceDays from sensor s 
-                                                   JOIN sensor2graph s2g on (s.sid = s2g.sensor)
-                                                   JOIN graph g on (s2g.graph = g.gid)
-                                                   where g.name = :name ');
-      
-      // bind variables and execute										
-      $stmt->execute(array(':name' => $graphName ));										
-		       
-      // create array
-      foreach ($stmt as $row) {		 
-         $data[$row[0]] = $row[1]; 
-      }
-      
-      return $data;
-      
-   } 
     
 }
 
